f802e297ceed592523e15d14ca36b66432a13786
complexitycollapse <complexitycollapse@protonmail.com>
2024-09-20 13:33:58 +0100

    Initial code

---
+++ README.md
@@ -0,0 +1,14 @@
+# Group Strategy Simulation
+
+This is a simulator for the iterated prisoner's dilemma game. What makes it different is that rather than having many bots trying to win the game as individuals, they are instead divided into "tribes", and the goal is to maximize the total score of the tribe rather than the individual.
+
+## Any Interesting Results?
+
+Here's one. Consider the following strategies:
+
+ - Tit-for-tat - the traditional tit-for-tat strategy, where you cooperate on the first turn and next time play whatever your opponent played in the last round
+ - In-group preference - cooperate with members of your tribe, and defect against all others.
+
+ Which is the superior strategy when they are pitted again each other? It depends on the numbers. When tit-for-tat is the majority, it is the most successful strategy. But when in-group preference is the majority, it will beat tit-for-tat. If they are equal, in-group preference is slightly prefered. This is similar to the well-known result for tit-for-tat vs. always defect in the non-tribal game - tit-for-tat wins unless it is heavily outnumbered by defect. The difference is that in-group preference can beat tit-for-tat even when the numbers are merely equal, making it a superior strategy to always defect in the tribal game.
+
+ Another (unsurprising) result is that a strategy of out-group preference (defect from your own tribe, cooperate with others) is an abysmal strategy in all cases.
---
+++ main.js
@@ -0,0 +1,150 @@
+function randInt(limit) {
+  return Math.floor(Math.random() * limit);
+}
+
+function distinctRandoms(limit) {
+  const firstIndex = randInt(limit);
+  let secondIndex = randInt(limit);
+  while (firstIndex === secondIndex) { secondIndex = randInt(limit)};
+
+  return [firstIndex, secondIndex];
+}
+
+function runRound(bots, histories) {
+  const [firstIndex, secondIndex] = distinctRandoms(bots.length);
+
+  const first = bots[firstIndex];
+  const second = bots[secondIndex];
+
+  const historyOfSecond = histories[firstIndex][secondIndex];
+  const historyOfFirst = histories[secondIndex][firstIndex];
+
+  const firstMove = first.move(first, second, historyOfSecond);
+  const secondMove = second.move(second, first, historyOfFirst);
+
+  if (firstMove !== "cooperate" && firstMove !== "defect") {
+    throw Error(`Bot with affiliation ${first.affiliation} played ${firstMove}`);
+  }
+
+  if (secondMove !== "cooperate" && secondMove !== "defect") {
+    throw Error(`Bot with affiliation ${second.affiliation} played ${secondMove}`);
+  }
+
+  if (firstMove === "cooperate" && secondMove === "cooperate") {
+    first.score += 1;
+    second.score += 1;
+  } else if (firstMove === "cooperate" && secondMove === "defect") {
+    first.score += -1;
+    second.score += 2;
+  } else if (firstMove === "defect" && secondMove === "cooperate") {
+    first.score += 2;
+    second.score += -1;
+  } else if (firstMove === "defect" && secondMove === "defect") {
+    first.score += 0;
+    second.score += 0;
+  }
+
+  historyOfSecond.unshift(secondMove);
+  historyOfFirst.unshift(firstMove);
+}
+
+function runRounds(bots, rounds, histories) {
+  for (let i = 0; i < rounds; ++i) {
+    runRound(bots, histories);
+  }
+}
+
+function createBots(botConfig) {
+  const bots = [];
+
+  botConfig.forEach(({botFn, instances}) => {
+    for (let i = 0; i < instances; ++i) {
+      const bot = botFn(i);
+      bot.score = 0;
+      bots.push(bot);
+    }
+  });
+
+  return bots;
+}
+
+function initHistories(count) {
+  const histories = new Array(count);
+  
+  for (let i = 0; i < count; ++ i) {
+    histories[i] = new Array(count);
+    for (let j = 0; j < count; ++ j) {
+      histories[i][j] = [];
+    }
+  }
+
+  return histories;
+}
+
+function playGame(scenario) {
+  const bots = createBots(scenario.botConfig);
+  const histories = initHistories(bots.length);
+  runRounds(bots, scenario.rounds, histories);
+  return bots.sort((a, b) => b.score - a.score);
+}
+
+function cooperateFn() {
+  return {
+    affiliation: "cooperate",
+    move: () => "cooperate"
+  };
+}
+
+function defectFn() {
+  return {
+    affiliation: "defect",
+    move: () => "defect"
+  };
+}
+
+function inGroup() {
+  return {
+    affiliation: "in-group",
+    move: (self, opponent) => opponent.affiliation === self.affiliation ? "cooperate" : "defect"
+  }
+}
+
+function outGroup() {
+  return {
+    affiliation: "out-group",
+    move: (self, opponent) => opponent.affiliation === self.affiliation ? "defect" : "cooperate"
+  }
+}
+
+function titForTat() {
+  return {
+    affiliation: "tit-for-tat",
+    move: (self, opponent, history) => history.length > 0 ? history[0] : "cooperate"
+  }
+}
+
+const scenario = {
+  rounds: 10000,
+  botConfig: [
+    { botFn: cooperateFn, instances: 10 },
+    { botFn: defectFn, instances: 100 },
+    { botFn: inGroup, instances: 1 },
+    { botFn: outGroup, instances: 10 },
+    { botFn: titForTat, instances: 10 }
+  ]
+}
+
+const result = playGame(scenario);
+
+const byAffiliation = new Map();
+
+result.forEach(bot => {
+  if (!byAffiliation.has(bot.affiliation)) {
+    byAffiliation.set(bot.affiliation, 0);
+  }
+
+  byAffiliation.set(bot.affiliation, byAffiliation.get(bot.affiliation) + bot.score);
+});
+
+console.log(JSON.stringify([...byAffiliation.entries()].sort((a, b) => b[1] - a[1])));
+console.log("total score:", [...byAffiliation.values()].reduce((a, b) => a + b, 0));
